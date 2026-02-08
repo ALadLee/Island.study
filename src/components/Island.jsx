87366a8d832fd86b3b6814538b48bba836b4ecf2
ALadLee <113834881+ALadLee@users.noreply.github.com>
2026-02-08 16:36:49 +0000

    Add Island visualization component

---
+++ src/components/Island.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Island = () => {
+    return (
+        <div className="island-visualization">
+            <h1>Island Visualization</h1>
+            {/* Add your island visualization logic here */}
+        </div>
+    );
+};
+
+export default Island;
