b5ef17fc6f18856138b09642849908bab5275456
ALadLee <113834881+ALadLee@users.noreply.github.com>
2026-02-08 16:37:02 +0000

    Create WeekInReview component

---
+++ src/components/WeekInReview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WeekInReview = () => {
+    return (
+        <div>
+            <h1>Week In Review</h1>
+            {/* Your weekly insights go here */}
+        </div>
+    );
+};
+
+export default WeekInReview;
