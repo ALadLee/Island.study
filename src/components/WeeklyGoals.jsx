040eb20c6165fb4f32d96af7087a0757017f3ee3
ALadLee <113834881+ALadLee@users.noreply.github.com>
2026-02-08 16:37:11 +0000

    Create WeeklyGoals component

---
+++ src/components/WeeklyGoals.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WeeklyGoals = () => {
+    return (
+        <div>
+            <h1>Weekly Goals</h1>
+            {/* Add your goals here */}
+        </div>
+    );
+};
+
+export default WeeklyGoals;
